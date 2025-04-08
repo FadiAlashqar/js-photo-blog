1cee54f4eb0330a0b61d62afb9d88ffc2963307a
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-04-08 12:38:08 +0200

    [UPD] milestone 3 done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const endpoint = `https://lanciweb.github.io/demo/api/pictures/`
 const cardImg = document.querySelectorAll('.card-img');
 const overlay = document.getElementById('overlay');
 const button = document.getElementById('button');
+const overlayImg = document.getElementById('overlay-img');
 
 axios.get(endpoint).then(res => {
     
@@ -13,6 +14,7 @@ axios.get(endpoint).then(res => {
         cardImg[i].innerHTML = `<img src="${url}" alt="">`
         cardImg[i].addEventListener('click', function(){
             overlay.classList.add('d-block');
+            overlayImg.innerHTML = `<img src="${url}" alt="">`
         });
         button.addEventListener('click', function() {
             overlay.classList.remove('d-block');
